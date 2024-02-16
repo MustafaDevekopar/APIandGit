75f8c878d4ad0f0ec52de7e92da413505cb9318f
mustafa <mustafadevelopar@gmail.com>
2024-02-16 14:22:35 +0300

    Add connection string in program file

--- APIandGit/Program.cs
+++ APIandGit/Program.cs
@@ -1,6 +1,12 @@
+using APIandGit.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(op=>
+op.UseSqlServer(builder.Configuration.GetConnectionString("MyCon")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
